842ef3826a7ccf6a90c29bd301335ce08ea8ce86
Jay Patrick Cano <0x3EF8@gmail.com>
2025-01-06 16:47:08 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/CodeTriad-Solutions' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/CodeTriad-Solutions' : '', 
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/CodeTriad-Solutions' : '', 
 };
 
 module.exports = nextConfig;
